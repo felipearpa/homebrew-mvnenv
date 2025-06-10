c04a9338e241fcc34b976e8271804b59f55cdcd4
Felipe Arcila Parra <felipearcila@gmail.com>
2025-06-09 19:41:16 -0500

    Update mvnenv version Update mvnenv to version 0.0.3 and refine installation process

--- Formula/mvnenv.rb
+++ Formula/mvnenv.rb
@@ -1,14 +1,15 @@
 class Mvnenv < Formula
   desc "Easily manage and switch between multiple Maven versions"
   homepage "https://github.com/felipearpa/mvnenv"
-  url "https://github.com/felipearpa/mvnenv/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "07714aab5226ca0a895c0c198c154e5cd2de413b95c4d962560e30714421a242"
+  url "https://github.com/felipearpa/mvnenv/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "55df24b56171e71698ee8a8e5f179add435a409894a21a55c0359edc732a00e8"
   license "LGPL-3.0"
   head "https://github.com/felipearpa/mvnenv.git"
 
   def install
-    libexec.install Dir["*"]
+    libexec.install Dir["*", "!test", "!test/*"]
     bin.write_exec_script libexec/"bin/mvnenv"
+    bin.write_exec_script libexec/"bin/mvn"
   end
 
   def caveats
